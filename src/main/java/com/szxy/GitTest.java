c8695032f3baf1abb8522c3817ea7f02325e1606
Dzq999 <3044973342@qq.com>
2023-05-07 17:15:31 +0800

    second commit

--- src/main/java/com/szxy/GitTest.java
+++ src/main/java/com/szxy/GitTest.java
@@ -3,5 +3,6 @@ package com.szxy;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Hello Git!!!");
     }
 }
